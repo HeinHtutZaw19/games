63a38eab238d179c594ccb85a3061ed299b6dcad
HeinHtutZaw16 <heinhtutzawhenry530@gmail.com>
2021-07-25 03:29:02 +0630

    AngryBirdsReplica basic movements finished

---
+++ minigames/AngryBirdsReplica/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private bool isPressed = false;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.15f);
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+        Debug.Log("Clicked");
+    }
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+        Debug.Log("Unclicked");
+    }
+
+    
+}
