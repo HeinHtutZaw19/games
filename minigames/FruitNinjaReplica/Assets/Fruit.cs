dba5304bc6d87ee684a05654ec4da46f37cacfd4
HeinHtutZaw16 <heinhtutzawhenry530@gmail.com>
2021-07-30 21:14:14 +0630

    FruitNinjaReplica blade added, watermelon added, bug in the slicing component

---
+++ minigames/FruitNinjaReplica/Assets/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject fruitSlicedPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Blade"))
+        {
+            Debug.Log("ok");
+            Vector3 direction = (collision.transform.position - transform.position).normalized;
+
+            Quaternion rotation = Quaternion.LookRotation(direction);
+
+            GameObject slicedFruit = Instantiate(fruitSlicedPrefab, transform.position, rotation);
+            Destroy(fruitSlicedPrefab,3f);
+            Destroy(gameObject);
+        }
+    }
+}
